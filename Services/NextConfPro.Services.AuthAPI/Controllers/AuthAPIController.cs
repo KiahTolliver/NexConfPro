6179a2e6e487c725c5471a44deb2283f854fad64
Kiah Imani <kiah@c-trax.com>
2024-02-07 11:12:57 -0500

    Added auth service to use .NET Identity

---
+++ Services/NextConfPro.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NextConfPro.Services.AuthAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+     [HttpPost("register")]
+     public async Task<IActionResult> Register()
+     {
+         return Ok();
+     }
+
+     [HttpPost("login")]
+     public async Task<IActionResult> Login()
+     {
+         return Ok();
+     }
+    }
+}
+
